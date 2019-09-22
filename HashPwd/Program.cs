cc3e4d71d814a47583730ca65962131fc32fdb5c
rjDevOnRun <john.76@gmail.com>
2019-09-22 10:32:02 +0900

    Initail CodeBase
    Methods to do string encryption

---
+++ HashPwd/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashPwd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pwdToUse = "Test123";
+
+            string hashedPwd = EncryptionHelper.Encrypt(pwdToUse);
+
+            string decrytedPwd = EncryptionHelper.Decrypt(hashedPwd);
+
+        }
+
+        private static void Method1(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine($"Insufficeint arguments provided. " +
+                    $"Usage for Encoding: HashPwd 'password_string' 'e' " +
+                    $"Usage for DeCoding: HashPwd 'password_string' 'd'"
+                    );
+                return;
+            }
+
+            // get password string
+            string inputString = args[0];
+            string actionToDo = args[1];
+
+            // create encoded/hashed password string
+            string encodedPassword = EncodeThisPassword(inputString);
+        }
+
+        private static string EncodeThisPassword(string stringToEncode)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(stringToEncode);
+            byte[] inArray = HashAlgorithm.Create("SHA1").ComputeHash(bytes);
+
+            return Convert.ToBase64String(inArray);
+        }
+    }
+}
